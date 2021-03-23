9716912f16dd203d2f377375a219723d834ca491
minatore06 <stei2002@hotmail.it>
2021-03-23 20:02:56 +0100

    finito

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var risposta
 function start()
 {
 var xhr = new XMLHttpRequest()
@@ -6,10 +7,10 @@ var xhr = new XMLHttpRequest()
 
   xhr.onreadystatechange = function() {
   	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-      let risposta = JSON.parse(xhr.response)
+      risposta = JSON.parse(xhr.response)
       console.log(risposta)
       risposta.forEach(impiegato => {
-        document.getElementById("tabella").innerHTML += "<tr><td>"+impiegato.firstName+" "+impiegato.lastName+"</td><td>"+impiegato.email+"</td><td>"+impiegato.phone+"</td><td><input type='button' value='mod' onclick='modifica("+impiegato.employeeId+")'><input type='button' value='rem' onclick='rimuovi("+impiegato.employeeId+")'></td></tr>"
+        document.getElementById("tabella").innerHTML += "<tr><td>"+impiegato.firstName+" "+impiegato.lastName+"</td><td>"+impiegato.email+"</td><td>"+impiegato.phone+"</td><td><input type='button' value='mod' onclick='vedi("+impiegato.employeeId+")'><input type='button' value='rem' onclick='rimuovi("+impiegato.employeeId+")'></td></tr>"
       });
       
 		}
@@ -17,10 +18,27 @@ var xhr = new XMLHttpRequest()
   xhr.send();
 }
 
-function aggiungi(impiegatoId, nome, cognome, email, phone){
+function vedi(aggiunta){
+  if(aggiunta === true)document.getElementById('conferma').setAttribute("onclick","inserimento()")
+  else document.getElementById('conferma').setAttribute("onclick","modifica("+aggiunta+")")
+  document.getElementById('aggiungi').style.display = "block";
+}
+
+function inserimento(){
   var xhr = new XMLHttpRequest()
   xhr.open("POST", "http://localhost:8080/api/tutorial/1.0/employees", true)
   xhr.setRequestHeader("Accept", "*/*")
+  xhr.setRequestHeader("Content-Type", "application/json")
+
+  let impiegatoId = 0;
+  risposta.forEach(impiegato => {
+    if(impiegato.employeeId>impiegatoId)impiegatoId = impiegato.employeeId;
+  })
+  impiegatoId+=1;
+  let nome = document.getElementById('nome').value; 
+  let cognome = document.getElementById('cognome').value; 
+  let email = document.getElementById('email').value; 
+  let phone = document.getElementById('telefono').value; 
 
   let data = {
     "employeeId": impiegatoId,
@@ -36,33 +54,54 @@ function aggiungi(impiegatoId, nome, cognome, email, phone){
       
 		}else if(xhr.readyState === xhr.DONE && xhr.status !== 201){
       document.getElementById("response").innerHTML = "Errore"
+      console.log(xhr.status)
     }
   };
 
-  xhr.send(data);
+  xhr.send(JSON.stringify(data));
 }
 
-function modifica(impiegatoId, nome, cognome, email, phone){
-  var xhr = new XMLHttpRequest()
-  xhr.open("PATH", "http://localhost:8080/api/tutorial/1.0/employees/"+impiegatoId, true)
+function modifica(impiegatoId){
+  let nome = document.getElementById('nome').value; 
+  let cognome = document.getElementById('cognome').value; 
+  let email = document.getElementById('email').value; 
+  let phone = document.getElementById('telefono').value; 
+
+  let xhr = new XMLHttpRequest()
+  xhr.open("GET", "http://localhost:8080/api/tutorial/1.0/employees/"+impiegatoId, true)
   xhr.setRequestHeader("Accept", "*/*")
 
-  let data = {};
-  if(nome)data.firstName = nome;
-  if(cognome)data.lastName = cognome;
-  if(email)data.email = email;
-  if(phone)data.phone = phone;
-
   xhr.onreadystatechange = function() {
-  	if(xhr.readyState === xhr.DONE && xhr.status === 200) {
-      window.location.reload();
+  	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+      risposta = JSON.parse(xhr.response)    
       
-		}else if(xhr.readyState === xhr.DONE && xhr.status !== 200){
-      document.getElementById("response").innerHTML = "Errore"
-    }
-  };
+      xhr = new XMLHttpRequest()
+      xhr.open("PATCH", "http://localhost:8080/api/tutorial/1.0/employees/"+impiegatoId, true)
+      xhr.setRequestHeader("Accept", "*/*")
+      xhr.setRequestHeader("Content-Type", "application/json")
 
-  xhr.send(data);
+      let data = {}
+      if(nome)data.firstName = nome;
+      if(cognome)data.lastName = cognome;
+      if(email)data.email = email;
+      if(phone)data.phone = phone;
+      console.log(data)
+
+      xhr.onreadystatechange = function() {
+        if(xhr.readyState === xhr.DONE && xhr.status === 200) {
+          window.location.reload();
+          
+        }else if(xhr.readyState === xhr.DONE && xhr.status !== 200){
+          document.getElementById("response").innerHTML = "Errore"
+        }
+      };
+
+      xhr.send(JSON.stringify(data));
+		}
+  };
+  xhr.send();
+
+  
 }
 
 function rimuovi(impiegatoId){
